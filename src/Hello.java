717e509b62e3777ff910bd92154c4cccb1ab6575
Gulbeyaz <gulbeyazt@gmail.com>
2020-11-10 12:49:10 -0500

    project is already in remote repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,9 @@ public class Hello {
         System.out.println("Adding a new file to git");
         System.out.println("This is something!! ");
         System.out.println("Mac and Windows users are happy so see commit diaglog finally! ");
+    //i have already published on github.com
+        //now i am ready to make more commits
+        //but do i need to share again??
+        //no, its time to commit and just push!!
     }
 }
