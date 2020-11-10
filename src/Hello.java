147dfd608fe0798c1879e5ebc0650e07efd794e8
Gulbeyaz <gulbeyazt@gmail.com>
2020-11-10 12:26:04 -0500

    added more line of code to Hello class

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Adding a new file to git");
         System.out.println("This is something!! ");
+        System.out.println("Mac and Windows users are happy so see commit diaglog finally! ");
     }
 }
