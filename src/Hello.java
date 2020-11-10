c2f79176e319d0df0d402b050a8560316c4ced40
Gulbeyaz <gulbeyazt@gmail.com>
2020-11-10 12:24:48 -0500

    added a line of code to second class

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Adding a new file to git");
+        System.out.println("This is something!! ");
     }
 }
